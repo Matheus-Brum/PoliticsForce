8f5b9070442a29cb7542dce48cb53761e0ca980c
George Ahmarani <george.ahmarani@hotmail.com>
2018-09-30 17:46:16 -0400

    index.py: starting page

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
-print('Hello World!')
+from .database import Database
+from flask import g
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
+
+
+@app.route('/')
+def start_page():
+    return render_template('acceuil.html')
