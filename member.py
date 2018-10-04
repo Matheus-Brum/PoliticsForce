1f954679da7d449e38e2bb07ba53c9553a96ab07
Matheus <brum.matheus@courrier.uqam.ca>
2018-10-04 10:14:52 -0400

    ajout de membres - better Members table and OOP approach to convert data into objects

---
+++ member.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self, f_name, l_name, member_no, phone_no, address):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.member_no = member_no
+        self.phone_no = phone_no
+        self.address = address
