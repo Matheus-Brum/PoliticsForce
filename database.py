9dc76d73a6d6999bb6892e9f8042f6bb8813838b
George Ahmarani <george.ahmarani@hotmail.com>
2018-10-07 13:54:17 -0400

    database.py: ajout de la suppr + modi. de get all

--- database.py
+++ database.py
@@ -24,17 +24,26 @@ class Database:
                        (member.f_name, member.l_name, member.member_no, member.phone_no, member.address))
         self.connection.commit()
 
+    def supprimer_member(self, member_number):
+        cursor = self.get_connection().cursor()
+        cursor.execute("DELETE FROM Members "
+                       "WHERE Member_no =? ", (member_number,))
+        self.connection.commit()
+
     def get_all_members(self):
-        counter = 1
         members = []
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT max(id) "
+        counter = 0
+        cursor.execute("SELECT count(*) "
                        "FROM Members")
         limit = cursor.fetchone()[0]
-        while counter <= limit:
+        cursor.execute("SELECT id "
+                       "FROM Members")
+        counters = cursor.fetchall()
+        while counter < limit:
             cursor.execute("SELECT * "
                            "FROM Members "
-                           "WHERE Id = ?", (counter,))
+                           "WHERE Id = ?", (counters[counter][0],))
             member_info = cursor.fetchone()
             member = Member(member_info[1], member_info[2], member_info[3], member_info[4], member_info[5])
             members.append(member)
