914d1cdb7fabfaf6802d9eb512070c491103649e
Claudiu Cobzaru <cobzaru.claudiu@courriel.uqam.ca>
2018-11-24 16:11:28 -0500

    Multilang support for template/confirmation

---
+++ language/confirmation.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from .layout import *
+
+confirmation_fr = {
+    "confirm": "Votre compte a été créé.",
+    "return": "Retour à la page d'accueil"
+}
+confirmation_content_fr = {**confirmation_fr, **layout_fr}
+
+confirmation_en = {
+    "confirm": "Your account has been created.",
+    "return": "Back to homepage"
+}
+confirmation_content_en = {**confirmation_en, **layout_en}
