b3785048e36e3e2e785f2eb161e682d11b77a865
Claudiu Cobzaru <cobzaru.claudiu@courriel.uqam.ca>
2018-11-24 16:15:05 -0500

    Multilang support for template/membres

---
+++ language/membres.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from .layout import *
+
+membres_fr = {
+    "page_title": "Membres",
+    "first_name": "Prénom",
+    "last_name": "Nom",
+    "member_id": "Numéro de Membre",
+    "phone_no": "Numéro de Téléphone",
+    "address": "Adresse",
+    "details": "Détail",
+    "remove": "Supprimer",
+    "member_detail": "Détails"
+}
+membres_content_fr = {**membres_fr, **layout_fr}
+
+membres_en = {
+    "page_title": "Members",
+    "first_name": "First name",
+    "last_name": "Last name",
+    "member_id": "Member id",
+    "phone_no": "Phone number",
+    "address": "Address",
+    "details": "Details",
+    "remove": "Remove",
+    "member_detail": "Details"
+}
+membres_content_en = {**membres_en, **layout_en}
