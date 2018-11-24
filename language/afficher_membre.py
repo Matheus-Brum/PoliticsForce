192222153ec087f2f35d6ffab5fe854a6de73b77
Claudiu Cobzaru <cobzaru.claudiu@courriel.uqam.ca>
2018-11-24 16:05:18 -0500

    Multilang support for template/afficher_membre

--- language/afficher_membre.py
+++ language/afficher_membre.py
@@ -1,10 +1,41 @@
-from .layout import Layout
+# coding=utf-8
+from .layout import *
 
-class AfficherMembre:
+afficher_membre_fr = {
+    "page_title": "Voici les détails du membre : ",
+    "first_name": "Prénom : ",
+    "last_name": "Nom : ",
+    "member_id": "Numéro de Membre : ",
+    "phone_no": "Numéro de Téléphone : ",
+    "date_membership_expiration": "Date d'expiration du membre : ",
+    "best_time_contact": "Meilleur moment pour joindre : ",
+    "birth_date": "Date de naissance : ",
+    "e-mail": "Courriel : ",
+    "if_donation": "À fait un don ? : ",
+    "last_donation": "Dernière donation : ",
+    "date_last_donation": "Date de dernière donation : ",
+    "election_year": "Année électoral ? : ",
+    "comment": "Commentaire : ",
+    "address": "Adresse : "
 
-    content_fr={"page_title":"Voici les détails du membres"}
-    lay_fr=Layout.layout_fr
-    lay_en=Layout.layout_en
-    fr=content_fr.update(lay_fr)
-    content_en={"page_title":"Member details"}
-    en=content_en.update(lay_en)
+}
+afficher_membre_content_fr = {**afficher_membre_fr, **layout_fr}
+
+afficher_membre_en = {
+    "page_title": "Member details : ",
+    "first_name": "First name : ",
+    "last_name": "Last name : ",
+    "member_id": "Member id : ",
+    "phone_no": "Phone number : ",
+    "date_membership_expiration": "Membership expiration date",
+    "best_time_contact": "Best time to contact :  ",
+    "birth_date": "Birth date : ",
+    "e-mail": "E-mail : ",
+    "if_donation": "Donated ? : ",
+    "last_donation": "Last donation : ",
+    "date_last_donation": "Date of last donation: ",
+    "election_year": "Election year ? : ",
+    "comment": "Comment : ",
+    "address": "Address : "
+}
+afficher_membre_content_en = {**afficher_membre_en, **layout_en}
