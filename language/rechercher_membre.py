7d9218c5260b37086ece8bccfff1366251d9d846
Claudiu Cobzaru <cobzaru.claudiu@courriel.uqam.ca>
2018-11-24 16:16:22 -0500

    Multilang support for template/rechercher_membre

---
+++ language/rechercher_membre.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+from .layout import *
+
+rechercher_membre_fr = {
+    "page_title": "Rechercher un membre",
+    "first_name": "Prénom",
+    "last_name": "Nom",
+    "member_id": "Numéro du membre",
+    "phone_no": "Numéro du telephone",
+    "address": "Adresse",
+    "btn_submit": "Rechercher",
+    "btn_reset": "Vider",
+    "search_result": "Résultat de la recherche",
+    "download": "Imprimer / Telecharger",
+    "result_first_name": "Prénom",
+    "result_last_name": "Nom",
+    "result_member_id": "Numéro de Membre",
+    "result_phone_no": "Numéro de Téléphone",
+    "result_address": "Adresse",
+    "result_details": "Détails",
+    "result_member_detail": "Voir détails",
+    "no_result": "Aucun résultat trouvé",
+    "error_invalid_selection": "Erreur: selection invalides",
+    "error_invalid_data": "Erreur: donnees recherches invalides"
+}
+rechercher_membre_content_fr = {**rechercher_membre_fr, **layout_fr}
+
+rechercher_membre_en = {
+    "page_title": "Search members",
+    "first_name": "First name",
+    "last_name": "Last name",
+    "member_id": "Member id",
+    "phone_no": "Phone number",
+    "address": "Address",
+    "btn_submit": "Search",
+    "btn_reset": "Reset",
+    "search_result": "Search results",
+    "download": "Print / Download",
+    "result_first_name": "First name",
+    "result_last_name": "Last name",
+    "result_member_id": "Member id",
+    "result_phone_no": "Phone number",
+    "result_address": "Address",
+    "result_details": "Details",
+    "result_member_detail": "Details",
+    "no_result": "No result found",
+    "error_invalid_selection": "Error: invalid selection",
+    "error_invalid_data": "Error: invalid search data"
+}
+rechercher_membre_content_en = {**rechercher_membre_en, **layout_en}
